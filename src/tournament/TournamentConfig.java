da3a9e4fb3cf779d9ffeac2da0a46a70685e70ea
ryan <rsgm123@gmail.com>
2014-09-07 16:12:16 -0500

    Removed the core module, also the example server is working

---
+++ src/tournament/TournamentConfig.java
@@ -0,0 +1,36 @@
+package tournament;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+class TournamentConfig extends Configuration {@NotEmpty
+private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+}
